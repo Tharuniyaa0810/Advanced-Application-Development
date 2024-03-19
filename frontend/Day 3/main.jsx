a5668cdd49df2260bf85ba486308b1424cc1bfee
Tharuniyaa Jayakanth <104960840+Tharuniyaa0810@users.noreply.github.com>
2024-03-19 16:29:17 +0530

    Add files via upload

---
+++ frontend/Day 3/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+import App from './App.jsx'
+import AdminPage from './pages/AdminPage.jsx'
+import VenuesPage from './components/venue.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>,
+)
